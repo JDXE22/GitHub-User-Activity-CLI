7825c63113a2ae13092785982c06f5422f5c6f9b
David <davidesparzaj22@gmail.com>
2025-06-18 19:46:58 -0500

    refactor(src/index.js):
    wrap async logic in IIFE for
    
    better top-level await handling and
    
    error management

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import { GitHubService } from "../services/githubService.js";
 
 const gitHubUsername = process.argv[2];
 
-
 if (!gitHubUsername) {
-  console.error('Please provide a GitHub username as an argument.');
+  console.error("Please provide a GitHub username as an argument.");
   process.exit(1);
 }
 
 const gitHubService = new GitHubService();
 
-try {
-    const getUserData = await gitHubService.getUserData(gitHubUsername)
+(async () => {
+  try {
+    const getUserData = await gitHubService.getUserData(gitHubUsername);
     // return displayUserData(getUserData);
-    
-} catch (error) {
-    
-    console.error('An error occurred:', error.message);
+  } catch (error) {
+    console.error("An error occurred:", error.message);
     process.exit(1);
-}
+  }
+})();
