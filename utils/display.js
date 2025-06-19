ca3287361d8481095bad6217054d9e46ec869710
David <davidesparzaj22@gmail.com>
2025-06-18 19:57:09 -0500

    refactor(utils/display.js):
    improve robustness and parameter
    
    clarity in Display class

---
+++ utils/display.js
@@ -0,0 +1,16 @@
+export class Display {
+    constructor(username){
+        this.username = username
+    }
+
+    displayUserData = ({userdata}) => {
+        console.log(`Recent events for user: ${this.username}`);
+        for (const event of userdata) {
+            const eventType = event.type;
+            const repoName = event.repo.name;
+            
+            console.log(`Event: ${eventType}, Repository: ${repoName}`);
+            
+        }
+    }
+}
